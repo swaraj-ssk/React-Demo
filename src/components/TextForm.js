04a5f145d198a1c8dd5d64533d95ca90deec4d39
SwarajKalbande <swarajkalbande123@gmail.com>
2024-06-19 15:43:01 +0530

    Added additional functionalities

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,22 +1,39 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
-    const [text, setText]= useState("Enter text here");
+    const [text, setText] = useState("Enter text here");
 
-    const toUpper = ()=>{
+    const toUpper = () => {
         setText(text.toUpperCase());
     }
 
-    const handleOnChange = (event)=>{
+    const toLower = () => {
+        setText(text.toLowerCase());
+    }
+    
+    const handleOnChange = (event) => {
         setText(event.target.value)
     }
 
     return (
-        <><h1>{props.heading}</h1><div>
-            <div class="mb-3">
-                <textarea class="form-control" id="myBox" rows="7" value={text} onChange={handleOnChange}></textarea>
+        <>
+            <div className="container my-3">
+                <h1>{props.heading}</h1>
+                <div>
+                    <div class="mb-3">
+                        <textarea class="form-control" id="myBox" rows="7" value={text} onChange={handleOnChange}></textarea>
+                    </div>
+                    <button className="btn btn-primary mx-1" onClick={toUpper}>Convert to Uppercase</button>
+                    <button className="btn btn-primary mx-1" onClick={toLower}>Convert to Lowercase</button>
+                </div>
             </div>
-            <button className="btn btn-primary" onClick={toUpper}>Convert to Uppercase</button>
-        </div></>
+            <div className="container my-3">
+                <h1>Your text summary </h1>
+                <p>{text.length} characters and {text.split(" ").length} number of words</p>
+                <p>Time to read : {0.08 * text.split(" ").length} minutes</p>
+                <h1>Preview</h1>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
