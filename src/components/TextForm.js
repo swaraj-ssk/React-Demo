d0ba3f969171ce6f7053e68b63556e6dd48eae99
SwarajKalbande <swarajkalbande123@gmail.com>
2024-06-20 19:49:08 +0530

    Removed bug which counted new line as new word

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,17 +40,17 @@ export default function TextForm(props) {
                     <div className="mb-3">
                         <textarea className="form-control" id="myBox" rows="7" value={text} style={{ backgroundColor: props.mode === 'dark' ? "#2b3035" : 'white', color: props.mode === 'dark' ? "white" : '#2b3035' }} onChange={handleOnChange}></textarea>
                     </div>
-                    <button className="btn btn-primary mx-1" onClick={toUpper}>Convert to Uppercase</button>
-                    <button className="btn btn-primary mx-1" onClick={toLower}>Convert to Lowercase</button>
-                    <button className="btn btn-primary mx-1" onClick={toCopy}>Copy to Clipboard</button>
-                    <button className="btn btn-primary mx-1" onClick={toClear}>Clear Text</button>
-                    <button className="btn btn-primary mx-1" onClick={removeWhiteSpaces}>Remove Extra Spaces</button>
+                    <button className="btn btn-primary mx-1 my-1" onClick={toUpper}>Convert to Uppercase</button>
+                    <button className="btn btn-primary mx-1 my-1" onClick={toLower}>Convert to Lowercase</button>
+                    <button className="btn btn-primary mx-1 my-1" onClick={toCopy}>Copy to Clipboard</button>
+                    <button className="btn btn-primary mx-1 my-1" onClick={toClear}>Clear Text</button>
+                    <button className="btn btn-primary mx-1 my-1" onClick={removeWhiteSpaces}>Remove Extra Spaces</button>
                 </div>
             </div>
             <div className="container my-3">
                 <h1>Your text summary </h1>
-                <p>{text.length} characters and {text.split(" ").filter(i => i !== "").length} number of words</p>
-                <p>Time to read : {0.08 * text.split(" ").filter(i => i !== "").length} minutes</p>
+                <p>{text.length} characters and {text.split(/\s+/).filter(i => i !== "").length} number of words</p>
+                <p>Time to read : {0.08 * text.split(/\s+/).filter(i => i !== "").length} minutes</p>
                 <h1>Preview</h1>
                 <p>{text}</p>
             </div>
