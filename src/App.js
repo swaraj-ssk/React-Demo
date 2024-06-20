707cd7a1eae16570d6cafb27966b9ca2c9ca5bf2
SwarajKalbande <swarajkalbande123@gmail.com>
2024-06-20 16:20:40 +0530

    Added Routes in Application

--- src/App.js
+++ src/App.js
@@ -2,44 +2,51 @@ import './App.css';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import { useState } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  // Link,
+} from "react-router-dom";
 
 function App() {
   //whether dark mode is enabled or not
-  const [mode, setMode]= useState('light');
+  const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert= (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg : message,
+      msg: message,
       type: type
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null);
     }, 1500)
   }
 
-  const toggleMode = () =>{
-    if(mode==='dark'){
+  const toggleMode = () => {
+    if (mode === 'dark') {
       setMode('light');
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
-    }else{
+    } else {
       setMode('dark')
-      document.body.style.backgroundColor="#0d1033f2";
+      document.body.style.backgroundColor = "#0d1033f2";
       showAlert("Dark mode has been enabled", "success");
     }
   }
   return (
     <>
-      <Navbar text="TextUtils" mode={mode} toggleMode={toggleMode}/> 
-      <Alert alert={alert} />
-      <div >
-        <TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />
-        {/* <About/> */}
-      </div>
-
+      <BrowserRouter>
+        <Navbar text="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route exact path="/" element={<TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
